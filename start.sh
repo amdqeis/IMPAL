b32141ca59121f4b9507d0f946f86d20537653cf
key <ahmad.qeis122@gmail.com>
2026-05-12 04:57:57 +0700

    feat: add UI Figma assets and create start script for backend and frontend

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting backend and frontend..."
+
+# Start Backend
+cd backend || exit
+source venv/bin/activate
+uvicorn main:app --reload --host 0.0.0.0 --port 8000 &
+
+# Balik ke root
+cd ..
+
+# Start Frontend
+cd frontend || exit
+npm run dev &
+
+# Menunggu semua proses
+wait
